1123e2d43f331b215f519ce0177201c615ccd8fd
Amanuel <amanuelbehailu557@gmail.com>
2026-01-24 13:33:05 +0300

    function for rotating

--- PRODIGY_WD_01/script.js
+++ PRODIGY_WD_01/script.js
@@ -1,8 +1,10 @@
 let cards = document.querySelectorAll(".cards");
 function rotateCards(){
   let angle = 0;
-  cards.forEach((card) => {
-    card.style.transform = 'rotate(${angle}deg)';
+  cards.forEach((card, index) => {
+    card.style.transform = `rotate(${angle}deg)`;
     angle = angle - 10;
+    card.style.zIndex = cards.length - index;
   })
 }
+document.addEventListener('DOMContentLoaded', rotateCards)
