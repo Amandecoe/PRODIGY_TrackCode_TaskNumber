b8ee0cd6c810a6c284fcded6b089ea68f6ac7dd7
Amanuel <amanuelbehailu557@gmail.com>
2026-01-27 10:54:11 +0300

    js part done for scroll animation

--- PRODIGY_WD_01/script.js
+++ PRODIGY_WD_01/script.js
@@ -60,3 +60,5 @@ const observer = new IntersectionObserver((entries)=> {
      }
   })
 })
+
+hiddenelements.forEach(el) = observer.observe(el); //loops over all elements saved in the hiddenelements variable
